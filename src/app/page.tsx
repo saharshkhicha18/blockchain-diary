d0ab88e59b75c7d437ff70b853f133c903830a4a
Saharsh Khicha <71884962+SaharshKhicha18@users.noreply.github.com>
2024-08-23 15:32:56 +0800

    change heading

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,7 +23,7 @@ export default function Home() {
       <div className="relative isolate min-h-[100dvh]">
         <main className="mx-auto max-w-screen-2xl py-16 px-8 md:px-24 tall:min-h-[calc(100dvh-128px)]">
           <h1 className="dark:text-white text-5xl md:text-6xl font-bold tracking-tight md:pt-24">
-            Demo App
+            Blockchain Diary/Note-Taker
           </h1>
           <p className="dark:text-white py-4 md:max-w-lg">
             Explore this demo app built with Next.js, Tailwind, and{' '}
